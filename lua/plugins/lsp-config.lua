86bd3f4bf35e78428b7a8167c4253fda420509e5
Hazem Mohamed <hazem@smartlara.com>
2024-01-16 20:32:44 +0300

    Add lazy git

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -34,7 +34,7 @@ return {
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set('n', '<leader>f', function()
+            vim.keymap.set('n', '<leader>ff', function()
                 vim.lsp.buf.format { async = true }
             end, opts)
         end
