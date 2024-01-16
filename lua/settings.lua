07656bf6a6cd4990207ef6d9e27aa7478d30abf9
Hazem Mohamed <hazem@smartlara.com>
2024-01-16 20:24:13 +0300

    Add toggle term

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,6 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set ts=4")
 vim.cmd("set sw=4")
 vim.cmd("set softtabstop=4")
+vim.cmd("set number")
 vim.cmd("syntax on")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
 
+-- Set leader to Space
 vim.g.mapleader = " "
